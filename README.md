1266ee51b0c79a38ad5db28e750d3c68ff4038d2
Alan Plum <ap@apsq.de>
2012-11-17 16:54:05 +0100

    Initial import.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 obs.js
 ======
 
-Minimalist observable properties
+Minimalist observable properties inspired by knockout.js.
+
+This module is AMD and CommonJS compatible and should work with Node.js and modern browsers.
+
+If you want to use the library in legacy browsers make sure to use an EcmaScript polyfill like [augment.js](http://augmentjs.com).
+
+The implementation is straightforward, but is currently lacking a test suite and thus should not be considered production ready.
+
+
+License
+-------
+
+The MIT license. For more information see LICENSE.
---
+++ obs.js
@@ -0,0 +1,117 @@
+/**
+* obs.js 0.1 Minimalist observable properties.
+* Copyright (c) 2012 Alan Plum
+* Licensed under the MIT license.
+**/
+(function(root, factory) {
+    if (typeof exports === 'object') {
+        factory(exports);
+    } else if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else {
+        factory(root.obs = {});
+    }
+}(this, function(exports) {
+    function PubSub() {
+        var _subscribers = [];
+        
+        this.subscribe = function(fn) {
+            _subscribers.push(fn);
+        };
+        
+        this.unsubscribe = function(fn) {
+            var i = _subscribers.indexOf(fn);
+            if (i === -1) {
+                return false;
+            }
+            _subscribers.splice(i, 1);
+            return true;
+        };
+        
+        this.publish = function(val) {
+            _subscribers.forEach(function(subscriber) {
+                subscriber(val);
+            });
+        };
+    }
+    
+    function extend(obj) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        
+        args.forEach(function(src) {
+            Object.keys(src).forEach(function(key) {
+                obj[key] = src[key];
+            });
+        });
+        
+        return obj;
+    }
+    
+    exports.prop = extend(
+        function(initialValue) {
+            var _value = initialValue;
+            
+            function prop() {
+                if (arguments.length) {
+                    _value = arguments[0];
+                    prop.publish(_value);
+                } else {
+                    return _value;
+                }
+            }
+            
+            PubSub.call(prop);
+            extend(prop, exports.prop.fn);
+            
+            return prop;
+        },
+        {fn: {__is_obs__: true}}
+    );
+    
+    exports.computed = extend(
+        function(fn, dependencies, lazy) {
+            if (!Array.isArray(dependencies)) {
+                dependencies = [dependencies];
+            }
+            
+            var _value, _onNotify, computed;
+            
+            if (lazy === true) {
+                var _changed = true;
+                _value = void(0);
+                _onNotify = function() {
+                    _changed = true;
+                };
+                computed = function() {
+                    if (_changed) {
+                        _value = fn();
+                        _changed = false;
+                        computed.publish(_value);
+                    }
+                    return _value;
+                };
+            } else {
+                _value = fn();
+                _onNotify = function() {
+                    _value = fn();
+                    computed.publish(_value);
+                };
+                computed = function() {
+                    return _value;
+                };
+            }
+            
+            PubSub.call(computed);
+            extend(computed, exports.computed.fn);
+            
+            dependencies.forEach(function(obs) {
+                if (obs && typeof obs.subscribe === 'function') {
+                    obs.subscribe(_onNotify);
+                }
+            });
+            
+            return computed;
+        },
+        {fn: {__is_obs__: true}}
+    );
+}));
